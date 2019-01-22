8992e9ac64ffb44560575ceb08dc1b4e25baac55
Daniel Luca <lucadanielcostin@gmail.com>
2019-01-22 09:31:31 +0200

    Change publish script to rebuild locally and copy to nginx

--- publish.sh
+++ publish.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-echo "Regenerating static content"
+set -x
+
 hugo
 
-echo "Adding content to ipfs"
-hash=`ipfs add -r -q public | tail -1`
-echo "Published to key $hash"
-
-echo "Caching content in Infura"
-curl "https://ipfs.infura.io:5001/api/v0/cat?arg=$hash/index.html&archive=false" -o /dev/null
+cp public/* /usr/share/nginx/html

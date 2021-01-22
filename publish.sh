bc0ee43c253f1aa466a834f6c75b2809f0283420
Daniel Luca <lucadanielcostin@gmail.com>
2021-01-22 18:42:33 +0200

    Add Bitcoin whitepaper.

--- publish.sh
+++ publish.sh
@@ -9,4 +9,8 @@ hugo
 # Copy Pentacorn
 cp pentacorn public/ -rfv
 
+# Copy Bitcoin whitepaper
+# Craig Wright sent a copyright infringement to some websites to remove the Bitcoin whitepaper and bitcoincore.org did it. Lol, lame.
+cp bitcoin public/ -rfv
+
 scp -r public/* daniel@papaya:$install_path/ 
